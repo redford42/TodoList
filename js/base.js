91442c5ad63e54976dc102ad651ecbf7c1f49794
redford42 <redfordevereaux@gmail.com>
2018-04-14 23:02:38 +0800

    2018/4/14 完成了删除的JS emmmm进度很慢

--- js/base.js
+++ js/base.js
@@ -2,7 +2,8 @@
 
     'use strict';
     var $form_add_task = $('#btn_add')
-        , task_list = {}
+        ,$delete_task
+        ,task_list = {}
         ;
 
 
@@ -15,18 +16,23 @@
         var $task_list = $('.tasks-list');
         $task_list.html('');
         for(var i = 0;i<task_list.length;i++){
-            var $task = render_task_tpl(task_list[i]);
+            var $task = render_task_item(task_list[i],i);
             $task_list.append($task);
         }
+
+        $delete_task = $('.action.delete');
+        listen_task_delete();
+
     }
     /*上面那个函数要用的模板*/
-    function render_task_tpl(data) {
+    function render_task_item(data,index) {
+        if(!data || !index) return;
         var list_item_tpl =
-            '<div class="task-item">' +
+            '<div class="task-item" data-index="'+ index +'">' +
             '<span><input type="checkbox"></span>' +
             '<span class="task-content">' + data.content + '</span>' +
             '<span class="fr">' +
-            '<span class="action"> 删除</span>' +
+            '<span class="action delete"> 删除</span>' +
             '<span class="action"> 详细</span>' +
             '</span>' +
             '</div>';
@@ -49,24 +55,59 @@
         /*将新Task存入StoreJS*/
         // console.log('new_task',new_task);
         if(add_task(new_task)){
-            render_task_list();
             $input.val('');
         }
     });
+    
+    function listen_task_delete() {
+
+        $delete_task.on('click',function () {
+            var $this = $(this);
+            var $item = $this.parent().parent();
+            var index = $item.data('index');
+            var tmp = confirm('确定删除？');
+
+            // tmp ? delete_task(index):null;
+            if(tmp){
+                delete_task(index);
+            }
+
+        })
+
+    }
+
+
+
     /*将新的task存入到store.js*/
     function add_task(new_task) {
         /*将新Task推入task_list*/
         task_list.push(new_task);
         /*更新localStore*/
-        store.set('task_list',task_list);
+        refresh_task_list();
         return true;
 
+    }
+    
+    function delete_task(index) {
+        /*如果没有index或index在tasklist中不存在*/
+        if(index == undefined || !task_list[index]) return;
+        delete task_list[index];
+        /*更新localStorage*/
+        refresh_task_list();
+    }
+
+    /*刷新localStorage数据并更新Div模板*/
+    function refresh_task_list() {
+        store.set('task_list',task_list);
+        render_task_list();
+        return true;
     }
 
     /*开机函数*/
     function init() {
         task_list = store.get('task_list') || [];
         if(task_list.length){render_task_list();}
+
     }
     
 
